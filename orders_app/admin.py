0941b5b6bbebcc84881d858fdc8e8c9c2e65b1f3
vitaliyrybkin <vitaliy.rybkin@yandex.ru>
2025-09-01 15:42:57 +0300

    feat(orders_app): register new admin models and update ProductWeightAdmin configuration
    Added admin registrations for `Stock`, `Truck`, `TruckType`, `TruckCapacity`, and `Driver` models. Updated `ProductWeightAdmin` to refine filters, search fields, and ordering.

--- orders_app/admin.py
+++ orders_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from orders_app.models import Product, ProductWeight, AppUnit
+from orders_app.models import AppUnit, Product, ProductWeight, Stock, Truck, TruckType, TruckCapacity, Driver
 
 
 class ProductUnitInline(admin.TabularInline):
@@ -36,9 +36,37 @@ class UnitAdmin(admin.ModelAdmin):
 @admin.register(ProductWeight)
 class ProductWeightAdmin(admin.ModelAdmin):
     list_display = ("product", "unit_shortcut", "kg_per_unit")
-    list_filter = ("product", "unit")
+    list_filter = ("product", "product_unit")
     search_fields = "product",
-    ordering = ("product", "unit")
+    ordering = ("product", "product_unit")
 
     def unit_shortcut(self, obj):
         return obj.unit.unit_shortcut
+
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    list_display = ("stock_name", "address", "description")
+    ordering = ("stock_name",)
+
+
+@admin.register(Truck)
+class TruckAdmin(admin.ModelAdmin):
+    list_display = ("carrier", "truck_type", "truck_capacity", "description")
+    ordering = ("carrier", "truck_type", "truck_capacity")
+
+
+@admin.register(TruckType)
+class TruckTypeAdmin(admin.ModelAdmin):
+    list_display = ("truck_type", "description")
+    ordering = ("truck_type",)
+
+
+@admin.register(TruckCapacity)
+class TruckCapacityAdmin(admin.ModelAdmin):
+    list_display = ("truck_capacity", "description")
+    ordering = ("truck_capacity",)
+
+@admin.register(Driver)
+class DriverAdmin(admin.ModelAdmin):
+    list_display = ("driver_name", "carrier")
