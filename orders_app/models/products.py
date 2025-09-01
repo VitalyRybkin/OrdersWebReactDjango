2234eda08b5e001938255d5ba9b1be04f6ba0695
vitaliyrybkin <vitaliy.rybkin@yandex.ru>
2025-09-01 11:02:53 +0300

    feat(orders_app): enhance Unit model and Product conversion logic
    Updated the `Unit` model to include a `to_kg_factor` field for unit conversions and enforced uniqueness on `unit_name` and `unit_shortcut`. Simplified the `Product` model by removing `report_unit` and `report_unit_ratio`, adding precise decimal field for `base_unit_weight`, and implementing a conversion method for unit transformations. Added migrations to reflect these changes.

--- orders_app/models/products.py
+++ orders_app/models/products.py
@@ -1,25 +1,30 @@
 from django.db import models
 
+from orders_app.models import Unit
+
 
 class Product(models.Model):
     product_name = models.CharField(max_length=100)
     base_unit = models.ForeignKey(
-        "Unit", on_delete=models.CASCADE, related_name="products_as_base_unit"
+        "Unit", on_delete=models.CASCADE, related_name="products"
     )
-    base_unit_weight = models.SmallIntegerField()
-    report_unit = models.ForeignKey(
-        "Unit", on_delete=models.CASCADE, related_name="products_as_report_unit"
+    base_unit_weight = models.DecimalField(
+        max_digits=12, decimal_places=6,
+        help_text="For example: 1 pcs = 0.5 kg, 1 kg = 1.0, 1 ton = 1000.0"
     )
-    report_unit_ratio = models.SmallIntegerField(null=True)
+
     palette_volume = models.SmallIntegerField(null=True)
     description = models.TextField(null=True, blank=True)
+
+    def convert(self, quantity: float, from_unit: "Unit", to_unit: "Unit") -> float:
+        """Converts a quantity from one unit to another based on their conversion factors to a base unit."""
+        kg = quantity * float(self.base_unit_weight) * float(from_unit.to_kg_factor)
+        return kg / float(self.base_unit_weight) / float(to_unit.to_kg_factor)
 
     def __str__(self):
         return (
             f"<Product product_name={self.product_name} "
             f"base_unit={self.base_unit} "
-            f"report_unit={self.report_unit}> "
-            f"report_unit_ratio={self.report_unit_ratio} "
             f"palette_volume={self.palette_volume} "
             f"description={self.description}"
         )
--- orders_app/models/units.py
+++ orders_app/models/units.py
@@ -2,6 +2,11 @@ from django.db import models
 
 
 class Unit(models.Model):
-    unit_name = models.CharField(max_length=20)
-    unit_shortcut = models.CharField(max_length=2)
+    unit_name = models.CharField(max_length=20,unique=True)
+    unit_shortcut = models.CharField(max_length=3, unique=True)
     description = models.TextField()
+
+    to_kg_factor = models.DecimalField(
+        max_digits=12, decimal_places=6,
+        help_text="How many kilograms are in 1 unit of this type."
+    )
