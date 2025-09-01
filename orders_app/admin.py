4087ac01982c73832ab2ea1f28a8623f882939c4
vitaliyrybkin <vitaliy.rybkin@yandex.ru>
2025-09-01 14:01:32 +0300

    feat(orders_app): register Product, Unit, and ProductWeight models in admin panel

--- orders_app/admin.py
+++ orders_app/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
 
-# Register your models here.
+from orders_app.models import Product, Unit, ProductWeight
+
+
+class ProductUnitInline(admin.TabularInline):
+    model = ProductWeight
+    extra = 0
+    fields = ("unit", "kg_per_unit")
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = (ProductUnitInline,)
+
+    list_display = ("product_name", "palette_volume", "description")
+    list_filter = ("product_name",)
+    search_fields = ("product_name",)
+    ordering = ("product_name",)
+    filter_horizontal = ()
+    fieldsets = ()
+    readonly_fields = ()
+
+
+@admin.register(Unit)
+class UnitAdmin(admin.ModelAdmin):
+    inlines = (ProductUnitInline,)
+
+    list_display = ("unit_name", "unit_shortcut", "to_kg_factor", "is_weight_based")
+    list_display_links = ("unit_name",)
+    list_filter = ("unit_name",)
+    search_fields = ("unit_name",)
+    ordering = ("unit_name",)
+
+
+@admin.register(ProductWeight)
+class ProductWeightAdmin(admin.ModelAdmin):
+    list_display = ("product", "unit_shortcut", "kg_per_unit")
+    list_filter = ("product", "unit")
+    search_fields = "product",
+    ordering = ("product", "unit")
+
+    def unit_shortcut(self, obj):
+        return obj.unit.unit_shortcut
