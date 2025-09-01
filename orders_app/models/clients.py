4b2a1cdfde00e376f4aec7b0cb10f13209521809
vitaliyrybkin <vitaliy.rybkin@yandex.ru>
2025-09-01 14:23:06 +0300

    feat(orders_app): rename Client to Customer and update related fields
    Renamed `Client` model to `Customer` for better consistency and clarity. Updated related fields across `Order` model and migrations, reflecting the changes in naming conventions. Included minor adjustments to model documentation and string representations.

--- orders_app/models/clients.py
+++ orders_app/models/clients.py
@@ -1,6 +1,19 @@
 from django.db import models
 
 
-class Client(models.Model):
-    client_name = models.CharField(max_length=100)
+class Customer(models.Model):
+    """
+    Represents a client entity in the database.
+
+    Class is used to store and manage information about a client,
+    including the client's name and an optional description.
+
+    :ivar customer: The name of the client.
+    :ivar description: Additional description or details about the client,
+                       which is optional.
+    """
+    customer = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"ПОКУПАТЕЛЬ: {self.client_name}"
--- orders_app/models/orders.py
+++ orders_app/models/orders.py
@@ -5,8 +5,43 @@ from django.db.models import F, Sum
 
 
 class Order(models.Model):
+    """
+    Represents an order placed by a user.
+
+    Handles the relationships between a user, customer, stock, carrier, driver, truck,
+    products, and the related order status. Allows for tracking of order creation and
+    updates, as well as maintaining additional data such as comments and confirmation files.
+
+    :ivar user: The user who placed the order.
+    :type user: ForeignKey
+    :ivar customer: The customer associated with the order.
+    :type customer: ForeignKey
+    :ivar stock: The stock involved in the order.
+    :type stock: ForeignKey
+    :ivar carrier: The carrier responsible for transporting the order.
+    :type carrier: ForeignKey
+    :ivar driver: The driver assigned to the order.
+    :type driver: ForeignKey
+    :ivar truck: The truck assigned to transport the order.
+    :type truck: ForeignKey
+    :ivar products: The products included in the order, linked through an intermediary
+        model `OrderItem`.
+    :type products: ManyToManyField
+    :ivar created_at: Timestamp when the order was created.
+    :type created_at: DateTimeField
+    :ivar updated_at: Timestamp when the order was last updated.
+    :type updated_at: DateTimeField
+    :ivar status: The current status of the order. Defaults to `ACTIVE`. Possible values
+        include: `ACTIVE`, `CANCELED`, and `DELIVERED`.
+    :type status: CharField
+    :ivar comment: Additional comments about the order. Can be `null` or left blank.
+    :type comment: TextField
+    :ivar confirmation_file: An optional file for confirming the order. Can be `null` or
+        left blank.
+    :type confirmation_file: FileField
+    """
     user = models.ForeignKey("auth.User", on_delete=models.PROTECT)
-    client = models.ForeignKey("Client", on_delete=models.PROTECT)
+    customer = models.ForeignKey("Customer", on_delete=models.PROTECT)
     stock = models.ForeignKey("Stock", on_delete=models.PROTECT)
     carrier = models.ForeignKey("Carrier", on_delete=models.PROTECT)
     driver = models.ForeignKey("Driver", on_delete=models.PROTECT)
@@ -35,22 +70,16 @@ class Order(models.Model):
 
     @property
     def items(self):
+        """Returns a queryset of order items related to this order."""
         return self.orderitem_set.select_related("product")
 
     @property
     def total_weight(self):
+        """Returns the total weight of the order in kg."""
         result = self.items.aggregate(
             total=Sum(F("product__base_unit_weight") * F("quantity"))
         )["total"]
         return result or Decimal("0.000")
 
     def __str__(self):
-        return (
-            f"<Order user={self.user} "
-            f"client={self.client} "
-            f"stock={self.stock} "
-            f"carrier={self.carrier} "
-            f"driver={self.driver} "
-            f"truck={self.truck} "
-            f"status={self.status}>"
-        )
+        return f"ЗАКАЗ: {self.id} (Покупатель: {self.customer.customer})"
